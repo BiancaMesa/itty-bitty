dd0e397a6578502c767c5a85b7391414a299f900
BiancaMesa <bianca.koraa@gmail.com>
2024-07-22 12:13:17 +0200

    dashboard successfully changed and form included

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { ref } from 'vue';
 import { useForm, usePage } from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
@@ -48,5 +48,39 @@ const shortenedUrl = usePage().props.value.shortenedUrl;
                 </div>
             </div>
         </div>
+    </AuthenticatedLayout>
+</template> -->
+
+
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <!-- <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div> -->
+        
+        <!-- // NEW - VERIFY VUE CODE// -->
+        <section class="py-12">
+            <form @submit.prevent="submitForm" >
+                <label for="long_url">Enter Long URL:</label>
+                <input type="text" v-model="longUrl" name="long_url" id="long_url">
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+
     </AuthenticatedLayout>
 </template>
