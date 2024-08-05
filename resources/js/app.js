6f514a72756b7e89dd7c282e3436b17046746a84
BiancaMesa <bianca.koraa@gmail.com>
2024-08-05 13:53:18 +0200

    Removed Ziggy references from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,6 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { createPinia } from 'pinia';
 
 // Import Font Awesome
@@ -27,7 +26,6 @@ createInertiaApp({
         const pinia = createPinia();
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .use(pinia)
             .component('font-awesome-icon', FontAwesomeIcon) // Register FontAwesomeIcon component globally
             .mount(el);
